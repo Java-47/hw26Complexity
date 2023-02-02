0fd7ffe8ffb1dc3947d2b0b0f1cabbdf4daff64f
Alex Kamenka <kamenka993@gmail.com>
2023-02-02 04:20:29 +0200

    A - "O(1) O(n)", B-"O(log(n)) O(log(n))", C-"O(n) O(1)"

--- src/dao/EntityImpl.java
+++ src/dao/EntityImpl.java
@@ -1,49 +1,83 @@
 package dao;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.TreeSet;
 import interfaces.Entity;
 import interfaces.EntityCollection;
 
-public class EntityImpl implements Entity {
-
-	private int value;
-
-	public EntityImpl(int value) {
-		this.value = value;
-	}
-
-	@Override
-	public int getValue() {
-		return value;
-	}
-}
-
-class ABC implements EntityCollection { 
+class A implements EntityCollection { // add-high , removeMax-low
 
 	private HashSet<Entity> entities;
 
-	private Entity maxEntity = null;
-
-	public ABC() {
+	public A() {
 		entities = new HashSet<>();
 	}
 
 	// O(1)
 	@Override
 	public void add(Entity entity) {
-		if (maxEntity == null) {
-			maxEntity = entity;
-		} else {
-			if (maxEntity.getValue() < entity.getValue()) {
-				maxEntity = entity;
+		entities.add(entity);
+	}
+
+	// O(n)
+	@Override
+	public Entity removeMaxValue() {
+		Entity maxValue = Collections.max(entities, (e1, e2) -> e1.getValue() - e2.getValue());
+		entities.remove(maxValue);
+		return maxValue;
+	}
+}
+
+class B implements EntityCollection { // add-medium , removeMax-medium
+
+	private TreeSet<Entity> entities;
+
+	public B() {
+		entities = new TreeSet<>((e1, e2) -> e1.getValue() - e2.getValue());
+	}
+
+	// O(log(n))
+	@Override
+	public void add(Entity entity) {
+		entities.add(entity);
+	}
+
+	// O(log(n))
+	@Override
+	public Entity removeMaxValue() {
+		return entities.pollLast();
+	}
+}
+
+class C implements EntityCollection { // add-Low, removeMax-High
+
+	private ArrayList<Entity> entities;
+
+	public C() {
+		entities = new ArrayList<Entity>();
+	}
+
+	// O(n)
+	@Override
+	public void add(Entity entity) {
+		if (entity != null) {
+
+			int index = Collections.binarySearch(entities, entity, (e1, e2) -> e1.getValue() - e2.getValue());
+			if (index >= 0) {
+
+			} else {
+				index = -Collections.binarySearch(entities, entity, (e1, e2) -> e1.getValue() - e2.getValue()) - 1;
+				entities.add(index, entity);
 			}
 		}
-		entities.add(entity);
+
 	}
 
 	// O(1)
 	@Override
 	public Entity removeMaxValue() {
-		entities.remove(maxEntity);
-		return maxEntity;
+		return entities.remove(entities.size() - 1);
 	}
 }
